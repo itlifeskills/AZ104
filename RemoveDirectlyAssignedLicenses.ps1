8cec587e9bd634da3b40e956ad86c92ddef195d8
itlifeskills <106455363+itlifeskills@users.noreply.github.com>
2025-06-13 19:46:23 -0500

    Add files via upload

---
+++ RemoveDirectlyAssignedLicenses.ps1
@@ -0,0 +1,6 @@
+﻿Connect-MgGraph -Scopes "Directory.Read.All", "User.Read.All"
+
+
+$P2SkuId = (Get-MgSubscribedSku | Select * | where {$_.SkuPartNumber -eq "AAD_PREMIUM_P2"}).SkuId
+
+Set-MgUserLicense -UserId "6ed9dfc0-b1c7-40dd-adfc-9b208fe05828" -RemoveLicenses @($P2SkuId) -AddLicenses @()
